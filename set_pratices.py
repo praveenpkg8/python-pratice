e61184c334dbd579385fc7eaee00d502add9b81b
Praveen Kumar G <praveenpkg8@gmail.com>
2018-07-22 13:05:58 +0530

    heloo

---
+++ set_pratices.py
@@ -0,0 +1,13 @@
+farm_animals = {"cat", "dog", "pig", "cow"}
+print(farm_animals)
+
+for animals in farm_animals:
+    print(animals)
+
+print("=" * 40)
+
+vechicle = set(["car", "bikr", "van"])
+print(vechicle)
+
+for mode in vechicle:
+    print(mode)
